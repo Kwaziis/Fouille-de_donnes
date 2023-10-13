190434304e4bcfe4ec3c00ee421cdd60b1d617a0
Dino <72475325+dinoclier72@users.noreply.github.com>
2023-10-13 13:58:16 +0200

    ajout du model de donnee

---
+++ model.py
@@ -0,0 +1,6 @@
+class Action:
+    def __init__(self,user,date,actionType,actionDetails):
+        self.user = user
+        self.date = date
+        self.actionType = actionType
+        self.actionDetails = actionDetails#dico qui contient attribut
