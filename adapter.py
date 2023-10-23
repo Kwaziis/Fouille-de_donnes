671f17585a8dce490293eb41ce72ba14865189f2
Kwaziis <glparthur03@gmail.com>
2023-10-23 21:02:53 +0200

    update adapter.py
    ca va chercher les infos ca le range dadns un obj de la classe action et ca ajoute le ttt a une liste nomée actions

--- adapter.py
+++ adapter.py
@@ -1,6 +1,6 @@
 import json
 import mysql.connector
-
+import model
 
 # Etape 1: Etablir une connexion vers la base de données
 
@@ -8,11 +8,35 @@ db_config = { 'host': 'localhost', 'user': 'root', 'password': '', 'database': '
 conn = mysql.connector.connect(**db_config)
 
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM  transition')
+
+
+
+
+cursor.execute("SELECT Utilisateur, Date, Heure, Titre, Delai, RefTran, Commentaire FROM transition")
+
+# Récupération des données et création d'instances de la classe Action
+actions = []
+for row in cursor.fetchall():
+    user, date, heure, titre, delai, reftran, commentaire = row
+    actionDetails = {'Delai': delai, 'RefTran': reftran, 'Commentaire': commentaire}
+    action = model.Action(user, date, titre, actionDetails)
+    actions.append(action)
+
+
+conn.close()
+
+#print pour le test
+for action in actions:
+    print(f"User: {action.user}, Date: {action.date}, ActionType: {action.actionType}")
+    print("ActionDetails:", action.actionDetails)
+    print()
+
+
+'''
 obj = cursor.fetchall()
 
 # Etape 2 :convertir les données en json
 dict_obj = { 'key': obj }
 json_obj = json.dumps(dict_obj,indent=4, sort_keys=True, default=str)
 
-print(json_obj)
+'''
