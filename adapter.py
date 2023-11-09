7f415e1b23dd9b2468a0ce436d057c87d391e248
Dino <72475325+dinoclier72@users.noreply.github.com>
2023-11-09 11:25:13 +0100

    modification du model et finission de l'adapter

--- adapter.py
+++ adapter.py
@@ -12,31 +12,38 @@ cursor = conn.cursor()
 
 
 
-cursor.execute("SELECT Utilisateur, Date, Heure, Titre, Delai, RefTran, Commentaire FROM transition")
+cursor.execute("SELECT Utilisateur, Date, Heure, Titre, Attribut FROM transition")
 
 # Récupération des données et création d'instances de la classe Action
 actions = []
 for row in cursor.fetchall():
-    user, date, heure, titre, delai, reftran, commentaire = row
-    actionDetails = {'Delai': delai, 'RefTran': reftran, 'Commentaire': commentaire}
-    action = model.Action(user, date, titre, actionDetails)
+    user, date, heure, titre, attribut = row
+    actionDetails = {}
+    if(attribut != ""):
+        attributs = attribut.split(",")
+        for detail in attributs:
+            details = detail.split("=")
+            if(len(details) == 1):
+                actionDetails[details[0]] = ""
+            else:
+                actionDetails[details[0]] = details[1]
+    action = model.Action(user, date,heure , titre, actionDetails)
     actions.append(action)
 
 
 conn.close()
 
+'''
 #print pour le test
 for action in actions:
     print(f"User: {action.user}, Date: {action.date}, ActionType: {action.actionType}")
     print("ActionDetails:", action.actionDetails)
     print()
-
-
 '''
-obj = cursor.fetchall()
 
-# Etape 2 :convertir les données en json
-dict_obj = { 'key': obj }
-json_obj = json.dumps(dict_obj,indent=4, sort_keys=True, default=str)
-
-'''
+# Etape 2: sauvegarder les actions dans un fichier json
+with open('actions.json', 'w') as f:
+    for action in actions:
+        action.date = str(action.date)
+        action.heure = str(action.heure)
+    json.dump(actions, f, default=lambda o: o.__dict__, indent=4)
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 class Action:
-    def __init__(self,user,date,actionType,actionDetails):
+    def __init__(self,user,date,heure,actionType,actionDetails):
         self.user = user
         self.date = date
+        self.heure = heure
         self.actionType = actionType
         self.actionDetails = actionDetails#dico qui contient attribut
